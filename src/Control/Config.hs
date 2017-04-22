1f488683f7a4a48cd2588c9b388cce8ec217a291
Sean Hess <seanhess@gmail.com>
2017-04-22 11:28:20 -0600

    much simlar env system

--- src/Control/Config.hs
+++ src/Control/Config.hs
@@ -2,32 +2,18 @@
 module Control.Config where
 
 import Prelude hiding (lines, readFile, drop)
-import Data.Map (Map, fromList, union)
+import Data.Maybe (fromMaybe)
 import Control.Monad.IO.Class (liftIO, MonadIO)
-import Data.Text (breakOn, unpack, lines, dropAround, drop)
-import Data.Text.IO (readFile)
-import qualified System.Environment as System
+import System.Environment (lookupEnv)
 
 
-type Environment = Map String String
+type Name = String
+type Default = String
+data Env = Env Name Default
 
-
-loadEnvSystem :: (MonadIO m) => m Environment
-loadEnvSystem = fromList <$> (liftIO $ System.getEnvironment)
-
-
-loadEnvFile :: (MonadIO m) => FilePath -> m Environment
-loadEnvFile = fmap (fromList . parseEnv) . liftIO . readFile
-  where
-    parseEnv = map (strings . breakOn "=") . lines
-    strings (a, b) = (unpack a, unpack $ stripQuotes $ drop 1 b)
-    stripQuotes = dropAround (== '"')
-
-
-loadEnv :: MonadIO m => FilePath -> m Environment
-loadEnv p = do
-    s <- loadEnvSystem
-    f <- loadEnvFile p
-    pure $ union s f
+loadEnv :: MonadIO m => Env -> m String
+loadEnv (Env n d) = do
+    mv <- liftIO $ lookupEnv n
+    pure $ fromMaybe d mv
 
 
